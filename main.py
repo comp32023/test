3b52350966900ca710cecf46c8a7824d6c663cf6
comp32023 <comp32023@mail.ru>
2023-09-12 13:17:05 +0300

    fastapi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from typing import Union
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
@@ -9,4 +10,49 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
 
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 3):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+
+# Тело запроса
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+app = FastAPI()
+
+
+#@app.post("/items/")
+#async def create_item(item: Item):
+#    return item
+
+@app.put("/items/{item_id}/{item2_id2}")
+async def create_item(item_id: int,item_id2: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
